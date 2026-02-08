33ffedd3958f205a55b3bccd7e680533531c21b1
Ajay-Kumar-13 <apulleti@gitam.in>
2026-02-08 23:39:18 +0530

    get data using useQuery

--- src/hooks/useAuthorities.js
+++ src/hooks/useAuthorities.js
@@ -4,6 +4,7 @@ import { fetchAuthorities } from "../services/fetchAuthorities";
 export const useAuthorities = () => {
     return useQuery({
         queryKey: ['fetch_authorities'],
-        queryFn: fetchAuthorities
+        queryFn: fetchAuthorities,
+        staleTime: 1000 * 60 * 30
     })
 }
--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,9 +1,40 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { fetchUsers } from "../services/fetchUsers";
+import { ACCESS_TOKEN, CRM_USERS_ENDPOINT } from "../constants";
 
 export function useUsers() {
     return useQuery({
         queryKey: ['fetch_users'],
-        queryFn: fetchUsers
+        queryFn: fetchUsers,
+        staleTime: 1000 * 60 * 30
+    })
+}
+
+export const CreateUser = () => {
+    return useMutation({
+        mutationFn: async(user) => {
+            const res = await fetch(`${CRM_USERS_ENDPOINT}/admin/users/newuser`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${ACCESS_TOKEN}`
+                },
+                body: user
+            })
+
+            if(!res.ok) throw new Error("Failed to create user!")
+            return res.json();
+        },
+        onMutate: () => {
+            // before request (optimistic update)
+        },
+        onSuccess: (data) => {
+            // success toast
+        },
+        onError: (error) => {
+            // error toast
+        },
+        onSettled: () => {
+            // always runs
+        }
     })
 }
